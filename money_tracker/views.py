9c8d502b15502ee0da566113b949cfc483e49c9d
Muhammad Iqbal Dwitama <muhammad.iqbal111@ui.ac.id>
2023-05-09 23:56:13 +0700

    add: create-flutter

--- money_tracker/views.py
+++ money_tracker/views.py
@@ -1,4 +1,5 @@
 import datetime
+import json
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.shortcuts import render
@@ -122,3 +123,22 @@ def create_transaction_ajax(request):
     
     context = {'form': form}
     return render(request, "create_transaction.html", context)
+
+@csrf_exempt
+def create_transaction_flutter(request):
+    if request.method == 'POST':
+        
+        data = json.loads(request.body)
+
+        new_transaction = TransactionRecord.objects.create(
+            name = data["name"],
+            type = data["type"],
+            amount = int(data["amount"]),
+            description = data["description"]
+        )
+
+        new_transaction.save()
+
+        return JsonResponse({"status": "success"}, status=200)
+    else:
+        return JsonResponse({"status": "error"}, status=401)
