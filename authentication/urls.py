9dd0134fe9e0b456336e184e6f2686239e3c2353
Muhammad Iqbal Dwitama <muhammad.iqbal111@ui.ac.id>
2023-05-10 00:53:08 +0700

    add things

--- authentication/urls.py
+++ authentication/urls.py
@@ -4,7 +4,6 @@ from authentication.views import *
 app_name = 'authentication'
 
 urlpatterns = [
-    # path('register/', register, name='register'),
     path('login/', login, name='login'),
     path('logout/', logout, name='logout'),
 ]
